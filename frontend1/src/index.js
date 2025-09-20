3e2455a9d5138a5dd0bdd22635eae0653a3ee388
Kittu-2006 <badakemrudula2006@gmail.com>
2025-09-20 07:46:24 +0530

    yes

--- frontend1/src/index.js
+++ frontend1/src/index.js
@@ -2,7 +2,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./ App";
+import App from "./App";   // ✅ no space
 import { AuthProvider } from "./context/AuthContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
