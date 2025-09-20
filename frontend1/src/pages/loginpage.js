126d77f52c63ed53d9a0069ec769c1cafd54c6b8
Kittu-2006 <badakemrudula2006@gmail.com>
2025-09-20 07:26:15 +0530

    yes

--- frontend1/src/pages/loginpage.js
+++ frontend1/src/pages/loginpage.js
@@ -1,6 +1,6 @@
 // src/pages/LoginPage.js
 import React, { useState } from "react";
-import api from "../api"; // ✅ fixed import
+import api from "../api/api"; // ✅ fixed import
 import { useAuth } from "../context/AuthContext";
 import { useNavigate } from "react-router-dom";
 
--- frontend1/src/pages/registerpage.js
+++ frontend1/src/pages/registerpage.js
@@ -1,6 +1,6 @@
 // src/pages/RegisterPage.js
 import React, { useState } from "react";
-import api from "../api"; // ✅ fixed import
+import api from "../api/api"; // ✅ fixed import
 import { useNavigate } from "react-router-dom";
 
 const RegisterPage = () => {
